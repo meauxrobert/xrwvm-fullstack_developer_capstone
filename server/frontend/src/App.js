2178816767303e0fe1fdc001708744dc43d204f6
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-04-29 15:43:31 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+import Register from "./components/Register/Register";
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 
@@ -5,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+      <Route path="/register" element={<Register />} />    </Routes>
   );
 }
 export default App;
