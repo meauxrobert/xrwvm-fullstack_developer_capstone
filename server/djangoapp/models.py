dbfcad305e20fd4b31a113452117c461a7f73201
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-05-13 17:09:53 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,20 +7,21 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, help_text='Enter the name')
     description = models.TextField()
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-    ]
+    year = models.IntegerField()
+    color = models.TextField()
+    type = models.TextField()
+
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -29,14 +30,10 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015),
-        ]
-    )
-# Other fields as needed
+    year = models.IntegerField(
+        default=2023,
+        validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    # Other fields as needed
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name  # Return the name as the string representation
