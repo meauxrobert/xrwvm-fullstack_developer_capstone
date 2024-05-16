a553be6d2aaea8fbbba1a29924fc1356cdcb2171
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-05-16 07:59:20 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-'https://robertmeaux-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://robertmeaux-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.
+                 proxy.cognitiveclass.ai']
 
-CSRF_TRUSTED_ORIGINS = ['https://robertmeaux-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://robertmeaux-8000.
+theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 REST_FRAMEWORK = {
@@ -97,7 +99,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -146,4 +149,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
