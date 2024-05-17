4b75e256905bfb40f078893ef22be7772da2b884
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-05-17 14:02:01 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -98,8 +98,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
         'NAME':
