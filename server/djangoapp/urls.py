3ceba5730534ed3a10ada718bba9741bae4b3bd3
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-05-17 13:44:12 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.views.generic import TemplateView  # Added because of the Error
+# from django.views.generic import TemplateView  # Added because of the Error
 
 app_name = 'djangoapp'
 urlpatterns = [
