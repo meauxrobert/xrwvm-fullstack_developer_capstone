ddbb2f45e8a6ac4863c323973967bd9228da85af
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-05-17 13:59:14 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -126,10 +126,10 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            JsonResponse = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status":
